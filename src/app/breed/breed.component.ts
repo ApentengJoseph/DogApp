3acc0d868b4856c9fedcc594e1c8825e1253c842
Apenteng Joseph <apentengjoseph59@gmail.com>
2022-10-08 13:41:09 +0100

    Updates of code

--- src/app/breed/breed.component.ts
+++ src/app/breed/breed.component.ts
@@ -17,14 +17,15 @@ export class BreedComponent implements OnInit {
     private dogApiService: DogapiService
   ) {}
 
-
   ngOnInit(): void {
     //Getting a snapshot of the route params(which is the name of the specified breed) so it can be passed to the getSpecifiedBreed() method  assign the data to the breed property on the component for binding on the template
     this.name = this.route.snapshot.paramMap.get('name');
-    this.sub = this.dogApiService.getSpecifiedBreed(this.name).subscribe((data: [] | any) => {
-      this.breed = data.message;
-    })
-
+    this.sub = this.dogApiService.getSpecifiedBreed(this.name).subscribe(
+      (data: [] | any) => {
+        this.breed = data.message;
+      },
+      (err) => console.log(err)
+    );
   }
 
   //This code below unsubcribes from the observable above to avoid memory leak.
--- src/app/sub-breed/sub-breed.component.ts
+++ src/app/sub-breed/sub-breed.component.ts
@@ -6,30 +6,31 @@ import { DogapiService } from '../dogapi.service';
 @Component({
   selector: 'app-sub-breed',
   templateUrl: './sub-breed.component.html',
-  styleUrls: ['./sub-breed.component.scss']
+  styleUrls: ['./sub-breed.component.scss'],
 })
 export class SubBreedComponent implements OnInit {
   name!: any;
   subBreed!: [] | any;
 
   private sub!: Subscription;
-  constructor(private route: ActivatedRoute,
-    private dogApiService: DogapiService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private dogApiService: DogapiService
+  ) {}
 
-
-    ngOnInit(): void {
-      //Getting a snapshot of the route params(which is the name of the specified su-breed) so it can be passed to the getSpecifiedSubBreed() method and assign the data to the suBreed property on the component for binding on the template
-      this.name = this.route.snapshot.paramMap.get('name');
-      this.sub = this.dogApiService.getSpecifiedSubBreed(this.name).subscribe((data: [] | any) => {
+  ngOnInit(): void {
+    //Getting a snapshot of the route params(which is the name of the specified su-breed) so it can be passed to the getSpecifiedSubBreed() method and assign the data to the suBreed property on the component for binding on the template
+    this.name = this.route.snapshot.paramMap.get('name');
+    this.sub = this.dogApiService.getSpecifiedSubBreed(this.name).subscribe(
+      (data: [] | any) => {
         this.subBreed = data.message;
-      })
-
-    }
-
-    //This code below unsubcribes from the observable above to avoid memory leak.
-    ngOnDestroy(): void {
-      this.sub.unsubscribe();
-    }
+      },
+      (err) => console.log(err)
+    );
   }
 
-
+  //This code below unsubcribes from the observable above to avoid memory leak.
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+}
